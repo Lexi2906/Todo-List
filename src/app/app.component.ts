8d57ded7f7d5416f53c35b32c61d041c71fbf3d0
Ola <olciakieryluk@gmail.com>
2024-12-01 12:19:19 +0100

    background-settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {TodoListComponent} from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoListComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  standalone: true,
+  styleUrl: './todo-list.component.scss'
+})
+export class TodoListComponent {
+
+}
